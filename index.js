cc43d1ab17ffe7f6543203d70f50ce24312f7fdd
Ralph Nicholson <nicholson.re@gmail.com>
2023-05-04 10:13:55 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++){
+    const j = word.length - 1 - i;
+    let startChar = word[i];
+    let endChar = word[j];
+    if (startChar !== endChar) return false;
+  }
+  return true;
 }
 
 /* 
